322e2c9c2d76c9b46cbc91491a43ba99b3ebf633
Sergei Morugin <cmorugin@gmail.com>
2020-06-14 16:24:23 +0300

    add UI

---
+++ Weather/LoginFormController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginFormController.swift
+//  Weather
+//
+//  Created by Matthew on 13.06.2020.
+//  Copyright © 2020 Ostagram Inc. All rights reserved.
+//
+
+import UIKit
+
+class LoginFormController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTexField: UITextField!
+    
+    @IBAction func signinPressed(_ sender: Any) {
+        print(#function)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
