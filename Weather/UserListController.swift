b0036edd0e660793085f12e4b96dcb1eb79f7019
Sergei Morugin <cmorugin@gmail.com>
2020-06-21 15:55:56 +0300

    add user photo

--- Weather/UserListController.swift
+++ Weather/UserListController.swift
@@ -55,6 +55,15 @@ class UserListController: UITableViewController {
         return cell
     }
     
+  
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let userIndex = userTableView.indexPathForSelectedRow else { return }
+        let userPhotosVC = segue.destination as! UserPhotosController
+        userPhotosVC.user = userList[userIndex.row]
+    }
+ 
+    
 
     /*
     // Override to support conditional editing of the table view.
@@ -91,14 +100,6 @@ class UserListController: UITableViewController {
     }
     */
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
---
+++ Weather/UserPhotoCell.swift
@@ -0,0 +1,15 @@
+//
+//  UserPhotoCell.swift
+//  Weather
+//
+//  Created by Matthew on 20.06.2020.
+//  Copyright © 2020 Ostagram Inc. All rights reserved.
+//
+
+import UIKit
+
+class UserPhotoCell: UICollectionViewCell {
+
+    @IBOutlet weak var userPhoto: UIImageView!
+    
+}
