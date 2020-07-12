d915638d8719bef5d6f1367cf0a2c9842d8ed912
Sergei Morugin <cmorugin@gmail.com>
2020-07-12 15:56:51 +0300

    add custom animation for navigation control

--- Weather/users/UserListController.swift
+++ Weather/users/UserListController.swift
@@ -15,6 +15,8 @@ class UserListController: UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet weak var letterPicker: LetterPicker!
     @IBOutlet weak var searchBar: UISearchBar!
     
+    
+    
     var tmpUsers = ["Albert Einstein", "Bill Gates", "Bill Gates","Bill Gates","Elon Musk","Elon Musk","Elon Musk","Elon Musk","Elon Musk", "Jeff Bezos", "Jeff Bezos", "Jeff Bezos", "Sergey Brin", "Sergey Brin", "Sergey Brin"]
     
     
@@ -75,6 +77,8 @@ class UserListController: UIViewController, UITableViewDelegate, UITableViewData
         letterPicker.letterPikerDelegate = self
         letterPicker.letterArray = Array(sectionList.keys).sorted()
         searchBar.delegate = self
+        
+        
     }
 
     // MARK: - Table view data source
@@ -144,3 +148,157 @@ extension UserListController: LetterPickerDelegate{
     }
 
 }
+
+class Custom2NavigationController: UINavigationController{}
+
+class CustomNavigationController: UINavigationController, UINavigationControllerDelegate {
+    
+    
+    let interactiveTransition = CustomInteractiveTransition()
+
+    func navigationController(_ navigationController: UINavigationController,
+                              interactionControllerFor animationController: UIViewControllerAnimatedTransitioning)
+                              -> UIViewControllerInteractiveTransitioning? {
+        return interactiveTransition.hasStarted ? interactiveTransition : nil
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        delegate = self
+    }
+    
+    func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        print(operation)
+        if operation == .push {
+            self.interactiveTransition.viewController = toVC
+            return CustomPushAnimator()
+        } else if operation == .pop {
+            if navigationController.viewControllers.first != toVC {
+                self.interactiveTransition.viewController = toVC
+            }
+            return CustomPopAnimator()
+        }
+        return nil
+    }
+
+}
+
+
+final class CustomPushAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 1
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        guard let source = transitionContext.viewController(forKey: .from) else { return }
+        guard let destination = transitionContext.viewController(forKey: .to) else { return }
+        
+        transitionContext.containerView.addSubview(destination.view)
+        transitionContext.containerView.sendSubviewToBack(destination.view)
+           
+        destination.view.frame = source.view.frame
+        destination.view.layer.anchorPoint = CGPoint(x: 1, y: 0)
+        destination.view.center = CGPoint(x: source.view.bounds.width, y: 0.0)
+        destination.view.transform = CGAffineTransform(rotationAngle: -CGFloat.pi/2)
+        
+        source.view.layer.anchorPoint = CGPoint(x: 1.0, y: 0.0)
+        source.view.center = CGPoint(x: source.view.bounds.width, y: 0.0)
+
+        UIView.animate(withDuration: self.transitionDuration(using: transitionContext),
+            animations: {
+                source.view.transform = CGAffineTransform(rotationAngle: CGFloat.pi/2)
+                destination.view.transform = .identity
+        },
+            completion: {finished in
+                if finished && !transitionContext.transitionWasCancelled {
+                    source.removeFromParent()
+                } else if transitionContext.transitionWasCancelled {
+                    destination.view.transform = .identity
+                }
+                transitionContext.completeTransition(finished && !transitionContext.transitionWasCancelled)
+           }
+       )
+    }
+}
+
+final class CustomPopAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.6
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        guard let source = transitionContext.viewController(forKey: .from) else { return }
+        guard let destination = transitionContext.viewController(forKey: .to) else { return }
+        
+        transitionContext.containerView.addSubview(destination.view)
+        transitionContext.containerView.sendSubviewToBack(destination.view)
+           
+        destination.view.frame = source.view.frame
+        destination.view.layer.anchorPoint = CGPoint(x: 1, y: 0)
+        destination.view.center = CGPoint(x: source.view.bounds.width, y: 0.0)
+        destination.view.transform = CGAffineTransform(rotationAngle: CGFloat.pi/2)
+        
+        source.view.layer.anchorPoint = CGPoint(x: 1.0, y: 0.0)
+        source.view.center = CGPoint(x: source.view.bounds.width, y: 0.0)
+           
+        UIView.animate(withDuration: self.transitionDuration(using: transitionContext),
+            animations: {
+                source.view.transform = CGAffineTransform(rotationAngle: -CGFloat.pi/2)
+                destination.view.transform = .identity
+        },
+            completion: {finished in
+                if finished && !transitionContext.transitionWasCancelled {
+                    source.removeFromParent()
+                } else if transitionContext.transitionWasCancelled {
+                    destination.view.transform = .identity
+                }
+                transitionContext.completeTransition(finished && !transitionContext.transitionWasCancelled)
+           }
+       )
+    }
+    
+}
+
+
+class CustomInteractiveTransition: UIPercentDrivenInteractiveTransition {
+
+    var viewController: UIViewController? {
+        didSet {
+            let recognizer = UIScreenEdgePanGestureRecognizer(
+                target: self,
+                action: #selector(handleScreenEdgeGesture(_:)))
+            recognizer.edges = [.top]
+            viewController?.view.addGestureRecognizer(recognizer)
+        }
+    }
+    
+    var hasStarted: Bool = false
+    var shouldFinish: Bool = false
+
+    @objc func handleScreenEdgeGesture(_ recognizer: UIScreenEdgePanGestureRecognizer) {
+        print(recognizer)
+        switch recognizer.state {
+        case .began:
+            self.hasStarted = true
+            self.viewController?.navigationController?.popViewController(animated: true)
+        case .changed:
+            let translation = recognizer.translation(in: recognizer.view)
+            let relativeTranslation = translation.x / (recognizer.view?.bounds.width ?? 1)
+            let progress = max(0, min(1, relativeTranslation))
+            
+            self.shouldFinish = progress > 0.33
+
+            self.update(progress)
+        case .ended:
+            self.hasStarted = false
+            self.shouldFinish ? self.finish() : self.cancel()
+        case .cancelled:
+            self.hasStarted = false
+            self.cancel()
+        default: return
+        }
+    }
+
+    
+}
+
