f13af2f78f619744ae3f8f72092c0f5ceea69f10
Sergei Morugin <cmorugin@gmail.com>
2020-11-02 12:26:36 +0300

    implement flyweight pattern

--- Weather/components/CustomGradientView.swift
+++ Weather/components/CustomGradientView.swift
@@ -11,8 +11,10 @@ import UIKit
 @IBDesignable class CustomGradientView: UIView {
      
     private var gradientLayer: CAGradientLayer = CAGradientLayer()
-    @IBInspectable var startColor: UIColor = #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1)
-    @IBInspectable var endColor: UIColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+    @IBInspectable var startColor = UIColor.defaultGradientStartColor
+    //#colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1)
+    @IBInspectable var endColor = UIColor.defaultGradientEndColor
+    //#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
--- Weather/components/LikesView.swift
+++ Weather/components/LikesView.swift
@@ -12,8 +12,10 @@ import UIKit
 
     @IBInspectable var likesCount: Int = 10
     
-    @IBInspectable var unlikedColor: UIColor = #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1)
-    @IBInspectable var likedColor: UIColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+    @IBInspectable var unlikedColor = UIColor.defaultUnlikedColor
+        //: UIColor = #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1)
+    @IBInspectable var likedColor = UIColor.defaultLikedColor
+        //: UIColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
     @IBInspectable var liked: Bool = false
     
     
